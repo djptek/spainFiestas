73d418d81bd57b34a17533625b4aa5ccc331fdb1
djptek <dominic.page@elastic.co>
2019-10-17 12:33:28 +0200

    updated kibana bulk file(s)

--- src/main/java/demos/KibanaObjectsLoader.java
+++ src/main/java/demos/KibanaObjectsLoader.java
@@ -3,28 +3,34 @@ package demos;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 class KibanaObjectsLoader {
     private static Logger logger = LogManager.getLogger(App.class);
+    private static String[] filenames = {
+            "src/main/resources/.kibana_1_bulk.ndjson",
+            "src/main/resources/.kibana_2_bulk.ndjson"};
     private static BulkResponseDumper bulkResponseDumper = new BulkResponseDumper(logger);
-    private static String filename = "src/main/resources/kibana_1_bulk.ndjson";
-    private static File file = new File(filename);
+    private static BulkResponse bulkResponse;
     private static DataLoader dataLoader;
+    private static RestHighLevelClient client;
 
     KibanaObjectsLoader(RestHighLevelClient client) {
-        this.dataLoader = new DataLoader(client, file, logger);
+        this.client = client;
     }
 
     static void load() throws IOException {
-        dataLoader.load();
+        for (String filename : filenames) {
+            logger.log(Level.INFO, "Loading Kibana Objects "+filename);
+            File file = new File(filename);
+            dataLoader = new DataLoader(client, file, logger);
+            bulkResponse =  dataLoader.load();
+            bulkResponseDumper.dump(bulkResponse);
+        }
     }
 
 }
